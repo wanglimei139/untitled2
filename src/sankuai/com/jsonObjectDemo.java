e45a748267d6732a306d2cf7b40f682be1394e16
wanglimei04 <wanglimei04@meituan.com>
2019-11-27 21:01:36 +0800

    Initial commit

---
+++ src/sankuai/com/jsonObjectDemo.java
@@ -0,0 +1,22 @@
+package sankuai.com;
+
+/**
+ * @author: wanglimei
+ * @date: 2019/11/25
+ * @protocol:
+ * @apiName:
+ * @description: 描述：JSONObject使用方法详解, JSONObject-lib包是一个beans,collections,maps,java arrays和xml和JSON互相转换的包
+ */
+public class jsonObjectDemo {
+    public void jsonToJava(){
+        /**
+         * json字符串转java代码
+         */
+        String strjava = "{\"passwrod\":"\123456"\,\"username\":\"wlm\"}";
+
+    }
+
+
+
+
+}
